54850bdd7f75e921be933fa13dd3d684f48eba99
Tomoyuki Sakurai <y@trombik.org>
2025-01-19 15:26:13 +0700

    refactor: create Glossaby::Preprocessor::Base

---
+++ lib/glossaby/preprocessor/base.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "pathname"
+
+module Glossaby
+  module Preprocessor
+    class Base
+      def initialize(file)
+        @file = Pathname(file)
+      end
+
+      def read
+        File.read(@file.realpath)
+      end
+
+      def process
+        raise "method process not implemented: #{self} must implemente `process` method"
+      end
+    end
+  end
+end
--- lib/glossaby/preprocessor/markdown.rb
+++ lib/glossaby/preprocessor/markdown.rb
@@ -1,21 +1,13 @@
 # frozen_string_literal: true
 
+require "glossaby/preprocessor/base"
 require "redcarpet"
 require "redcarpet/render_strip"
-require "pathname"
 
 module Glossaby
   module Preprocessor
     # Implements Makrdown Preprocessor
-    class Markdown
-      def initialize(file)
-        @file = Pathname(file)
-      end
-
-      def read
-        File.read(@file.realpath)
-      end
-
+    class Markdown < Glossaby::Preprocessor::Base
       def process
         markdown = read
         Redcarpet::Markdown.new(Redcarpet::Render::StripDown).render(markdown)
