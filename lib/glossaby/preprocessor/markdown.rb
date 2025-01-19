baf450f26a4133f50c2fdb28a56b38bb1ca4141d
Tomoyuki Sakurai <y@trombik.org>
2025-01-20 06:24:51 +0700

    bugfix: ignore code blocks in Markdown preprocessor
    to prevent pollution

--- lib/glossaby/preprocessor/markdown.rb
+++ lib/glossaby/preprocessor/markdown.rb
@@ -4,13 +4,22 @@ require "glossaby/preprocessor/base"
 require "redcarpet"
 require "redcarpet/render_strip"
 
+# A custom render to ignore code blocks
+class StripDownWithoutCodeBlock < Redcarpet::Render::StripDown
+  # it is rare for code blocks to contain meaningfully necessary terms and
+  # the code pollutes glossary. ignore all the code blocks
+  def block_code(_code, _language)
+    nil
+  end
+end
+
 module Glossaby
   module Preprocessor
     # Implements Makrdown Preprocessor
     class Markdown < Glossaby::Preprocessor::Base
       def process
         markdown = read
-        Redcarpet::Markdown.new(Redcarpet::Render::StripDown).render(markdown)
+        Redcarpet::Markdown.new(StripDownWithoutCodeBlock, fenced_code_blocks: true).render(markdown)
       end
     end
   end
--- spec/glossaby/preprocessor/markdown_spec.rb
+++ spec/glossaby/preprocessor/markdown_spec.rb
@@ -5,6 +5,14 @@ require "glossaby/preprocessor/markdown"
 RSpec.describe Glossaby::Preprocessor::Markdown do
   let(:preprocessor) { described_class.new("/foo") }
   let(:content) { "# foo\nbar\n" }
+  let(:content_with_code_block) do
+    "# foo
+bar
+
+```ruby
+buz
+```"
+  end
 
   describe "#new" do
     it "does not throw" do
@@ -15,6 +23,11 @@ RSpec.describe Glossaby::Preprocessor::Markdown do
   describe "#process" do
     it "renders plain text" do
       allow(preprocessor).to receive(:read).and_return(content)
+      expect(preprocessor.process).to eq "foo\nbar\n"
+    end
+
+    it "renders plain text without code block" do
+      allow(preprocessor).to receive(:read).and_return(content_with_code_block)
       expect(preprocessor.process).to eq "foo\nbar\n"
     end
   end
